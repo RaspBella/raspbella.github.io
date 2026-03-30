9dfdeb458221b86e7e1c7019789c3fe00bd78dac
Bella Campbell <Bella.Campbell@tuta.io>
2026-03-30 15:07:00 +0100

    oh yeah c sucks

--- src/gen.c
+++ src/gen.c
@@ -61,21 +61,21 @@
 
 #define REDIRECT(URL) "<meta http-equiv=\"Refresh\" content=\"0; " URL "\" />"
 
-#define TEMPLATE "<!DOCTYPE html>\n"
-"<html lang=\"en\">\n"
-"  <head>\n"
-"    <title>%s</title>\n"
-"    <link rel=\"stylesheet\" href=\"/main.css\">\n"
-"    <meta charset=\"UTF-8\">\n"
-"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-"    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/favicon/apple-touch-icon.png\">\n"
-"    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon/favicon-32x32.png\">\n"
-"    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon/favicon-16x16.png\">\n"
-"    <link rel=\"manifest\" href=\"/favicon/site.webmanifest\">\n"
-"  </head>\n"
-"  <body>\n"
-"    %s\n"
-"  </body>\n"
+#define TEMPLATE "<!DOCTYPE html>\n" \
+"<html lang=\"en\">\n" \
+"  <head>\n" \
+"    <title>%s</title>\n" \
+"    <link rel=\"stylesheet\" href=\"/main.css\">\n" \
+"    <meta charset=\"UTF-8\">\n" \
+"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" \
+"    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/favicon/apple-touch-icon.png\">\n" \
+"    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon/favicon-32x32.png\">\n" \
+"    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon/favicon-16x16.png\">\n" \
+"    <link rel=\"manifest\" href=\"/favicon/site.webmanifest\">\n" \
+"  </head>\n" \
+"  <body>\n" \
+"    %s\n" \
+"  </body>\n" \
 "</html>"
 
 int main(void) {
